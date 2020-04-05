63a1da3abe4d3b37d8c19869884f3f3e2dbb8f0f
Jan G <apollg3@gmail.com>
2020-04-05 20:10:20 +0800

    Password Reset Flow

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 
 const Mutations = {
   // TODO: Check if user is logged in
@@ -7,7 +9,7 @@ const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
       {
-        data: { ...args }
+        data: { ...args },
       },
       info
     );
@@ -27,8 +29,8 @@ const Mutations = {
       {
         data: updates,
         where: {
-          id: args.id
-        }
+          id: args.id,
+        },
       },
       info
     );
@@ -60,8 +62,8 @@ const Mutations = {
         data: {
           ...args,
           password,
-          permissions: { set: ["USER"] }
-        }
+          permissions: { set: ["USER"] },
+        },
       },
       info
     );
@@ -72,12 +74,104 @@ const Mutations = {
     // Set jwt as a token on the response
     ctx.response.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24
+      maxAge: 1000 * 60 * 60 * 24,
     });
 
     // Return User
     return user;
-  }
+  },
+
+  async signin(parent, { email, password }, ctx, info) {
+    // Check user
+    const user = await ctx.db.query.user({ where: { email } });
+
+    if (!user) {
+      throw new Error(`No such user found for email: ${email}`);
+    }
+
+    // Check password
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error(`Invalid Password`);
+    }
+
+    // Generate jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // Set cookie with token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    // Return user
+    return user;
+  },
+
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Signed out." };
+  },
+
+  async requestReset(parent, args, ctx, info) {
+    // Check user
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+
+    if (!user) {
+      throw new Error(`No such user found for email: ${args.email}`);
+    }
+
+    // Set reset token and expiry
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000;
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    console.log(res);
+
+    return { message: "Reset Token Sent" };
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    // Check if passwords matched
+    if (args.password !== args.confirmPassword) {
+      throw new Error(`Passwords do not match.`);
+    }
+
+    // Check token
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    });
+
+    if (!user) {
+      throw new Error(`This token is either invalid or expired.`);
+    }
+
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    return updatedUser;
+  },
 };
 
 module.exports = Mutations;
